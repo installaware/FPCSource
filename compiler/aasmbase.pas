e6349fbe952d90684b9f2dbc9bfc456ea32a4e83
Don <LongDirtyAnimAlf@gmail.com>
2025-09-29 12:29:53 +0200

    Adjust TAsmSectiontype.

--- compiler/aasmbase.pas
+++ compiler/aasmbase.pas
@@ -99,6 +99,7 @@ interface
          sec_user,
          sec_code,
          sec_data,
+         sec_xdata,
          { read-only, but may contain relocations }
          sec_rodata,
          { read-only and cannot contain relocations }
